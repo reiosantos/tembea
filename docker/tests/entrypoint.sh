f0648caa50db889e83c5a4a57c63d7a2c6a625d3
Thegaijin <ssemagandavictor@gmail.com>
2018-11-20 21:47:40 +0300

    chore(docker): add docker images
            - add test docker image
            - add staging and production docker image
            - add entrypoint.sh file for testing locally
            - add docker-compose file for testing locally
            - add sequelizerc.build file for the production image
            - add pr template file
    
    why: containerise the application for deployment and development
    
    [Finishes #161982872]

---
+++ docker/tests/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+printf "\n\n=======================================\n"
+printf "Linting"
+printf "\n=======================================\n\n"
+yarn lint
+
+printf "\n\n=======================================\n"
+printf "Running tests"
+printf "\n=======================================\n\n"
+export NODE_ENV=test
+yarn test
+
+exit 0
