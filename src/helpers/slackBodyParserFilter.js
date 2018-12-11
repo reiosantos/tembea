0437ec34b480f263e6d38e054634afe664e3946c
victor chibuike <victorblackshady@gmail.com>
2018-12-11 11:48:58 +0100

    bug(slack): Fix bodyParser on slack command
    [Finishes #162574493]

--- src/helpers/slackBodyParserFilter.js
+++ src/helpers/slackBodyParserFilter.js
@@ -1,7 +1,7 @@
 export default class SlackBodyParserFilter {
   static maybe(fn) {
     return (req, res, next) => {
-      if (req.path.indexOf('/slack/') !== -1) {
+      if (req.path.indexOf('/slack/actions') !== -1) {
         next();
       } else {
         fn(req, res, next);
