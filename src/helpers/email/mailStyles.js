05d74d205877df925c91234ede92a98b70841024
Onwuzor Victor <onwuzorvictor.com>
2018-12-12 23:57:46 +0100

    feat(email-template): create email template for monthly trip report
    - tripReportMail is a method for trip report mails
    - displayDepartment get all departments from the db and display it
    - MailTemplate handles all mail template task
    
    [Finishes #162507111]

---
+++ src/helpers/email/mailStyles.js
@@ -0,0 +1,47 @@
+const mailStyles = {
+  app: `background-color: #ececec;
+    margin: 0;
+    padding-top: 80px;
+    font-weight: 100;
+    width: 100%;`,
+  content: `background-color: #FFFFFF;
+    width: 85%;
+    border: 1px solid #F5F5F5;
+    padding: 20px;
+    margin: 0 auto;`,
+  imageHolder: `color: #365ddc;
+    font-size: 27px;
+    font-weight: bold;
+    text-align: center;`,
+  image: 'height: 60px;',
+  title: `font-weight: 500;
+    margin-top: 30px;
+    color: #333E44;
+    text-align: center;`,
+  body: `font-size: 16px;
+    line-height: 21px;
+    color: #4F4F4F;
+    margin-bottom: 30px;
+    text-align: center;
+    font-weight: 700;`,
+  titleBar: `background: #FFAF30;
+    font-weight: 600;
+    color: #365ddc;`,
+  display: 'padding: 10px;',
+  tripsNumber: 'margin-right: 10px;',
+  total: `margin-top: 20px;
+    border-bottom: 2px solid rgba(189, 189, 189, 0.973);
+    border-top: 2px solid rgba(189, 189, 189, 0.973);
+    font-weight: 600;`,
+  compare: `margin-top: 50px;
+    margin-bottom: 20px;`,
+  float: 'float: right;',
+  footer: `
+    width: 60%;
+    margin: 0 auto;
+    padding: 10px;`,
+  h3: 'font-weight: 600;',
+  heading: 'letter-spacing: 4px;'
+};
+
+export default mailStyles;
