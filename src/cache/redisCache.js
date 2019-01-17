81f600783d9cb9b62f5d3d1628865ecc49170479
Allan Imire <imireallan@gmail.com>
2019-01-17 14:47:22 +0300

    fix(redis-env-variable): add redis environment variable
    - modify the redis singleton class to accept a remote url
    
    [Fixes #163287641]

--- src/cache/redisCache.js
+++ src/cache/redisCache.js
@@ -1,12 +1,13 @@
 import redis from 'redis';
 import util from 'util';
+import env from '../config/environment';
 
 class RedisCacheSingleton {
   constructor() {
     if (RedisCacheSingleton.exists) {
       return RedisCacheSingleton.instance;
     }
-    this.client = redis.createClient();
+    this.client = redis.createClient(env.REDIS_URL);
     this.getAsync = util.promisify(this.client.get).bind(this.client);
     RedisCacheSingleton.instance = this;
     RedisCacheSingleton.exists = this;
--- src/config/environment.js
+++ src/config/environment.js
@@ -6,7 +6,8 @@ const env = {
   DATABASE_URL: process.env.DATABASE_URL,
   DATABASE_DIALECT: process.env.DATABASE_DIALECT,
   BUGSNAG_API_KEY: process.env.BUGSNAG_API_KEY,
-  NODE_ENV: process.env.NODE_ENV || 'production'
+  NODE_ENV: process.env.NODE_ENV || 'production',
+  REDIS_URL: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
 };
 
 module.exports = envExists(env);
