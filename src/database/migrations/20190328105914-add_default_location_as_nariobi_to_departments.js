1cb04a011bf06358504647b065707bdded8b3163
muhwezi deo <aggrey256@gmail.com>
2019-03-28 15:25:04 +0300

    chore(database):add nairobi as default for location in Departments table
    - create migration file to add Nairobi as default for locations
    in Departments table
    
    [Delivers #164954365]

---
+++ src/database/migrations/20190328105914-add_default_location_as_nariobi_to_departments.js
@@ -0,0 +1,9 @@
+module.exports = {
+  up: queryInterface => queryInterface.sequelize.query(`ALTER TABLE "Departments"
+   ALTER COLUMN location SET DEFAULT 'Nairobi'; UPDATE "Departments" 
+   SET location = 'Nairobi' WHERE location is NULL;`),
+
+  down: queryInterface => queryInterface.sequelize.query(`ALTER TABLE "Departments"
+  ALTER COLUMN location DROP DEFAULT`)
+  
+};
