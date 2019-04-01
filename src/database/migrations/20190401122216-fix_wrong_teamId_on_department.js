5e86da14d58bb919924f8c61fb01cba73858fa33
Mubarak Imam <mubarak.imam@andela.com>
2019-04-01 13:43:18 +0100

    fix(department): fix bot not working on production

---
+++ src/database/migrations/20190401122216-fix_wrong_teamId_on_department.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {
+  up: (queryInterface) => {
+    if (process.env.NODE_ENV === 'production') {
+      queryInterface.sequelize.query(`
+        UPDATE "Departments" SET "teamId" = 'T02R3LKBA'
+        WHERE "Departments"."teamId" = 'TE2K8PGF8';`);
+    }
+    return Promise.resolve();
+  },
+
+  down: () => {}
+};
